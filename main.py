6e9b1b1bb0a930aa504d9a9345856e0912951d90
Zelenskiy <zelensk1973@gmail.com>
2025-02-16 16:47:41 +0200

    deploy

--- main.py
+++ main.py
@@ -9,12 +9,3 @@ if __name__ == "__main__":
 
     utils.getTable('1Dk0WYpOKeRoDATgzMkIkFjUcFwNAG5MRn4W7bEyzd0M')
 
-    # listStr = 'https://drive.google.com/open?id=10E28ixlBycFTNSkgn23NhrT8dGdi2nU1, https://drive.google.com/open?id=1SV-ef7siZH_n3J7o85E2SRniEPOKLfqR, https://drive.google.com/open?id=1i3AYLGXizuBHBPsvI-gVdkAuDc1p88Kn, https://drive.google.com/open?id=1dsjwhEmjgOkcheXtot7VAZ0mxIV_MYdS, https://drive.google.com/open?id=1FRYxu7sjr_-E9eD40ZxEUXKo6Daz5ZT8'
-    # listt = listStr.split(', ')
-    # for index, item in enumerate(listt):
-    #     url = "item"
-    #     name = utils.download_file_from_google_drive(item, "downloaded_files", str(index))
-    #     print(name)
-
-    # s = utils.process_downloaded_files()
-    # print(s)
