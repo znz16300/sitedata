193daadab1a063b2535ebda0640a479b8b0aaaf0
Zelenskiy <zelensk1973@gmail.com>
2025-02-17 20:45:34 +0200

    deploy

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import utils
 # Приклад використання:
 if __name__ == "__main__":
     
-    # utils.getTable('1F6QVr9WNio-_ODmnIlMTSHeSQxLOjgnd0nYB1_z0BeI')
+    utils.getTable('1F6QVr9WNio-_ODmnIlMTSHeSQxLOjgnd0nYB1_z0BeI')
     # utils.getTable('1G1l3J4HHLOItVLYbrPL08ml3TtON_fAULcpecqn0vwM')
     # utils.getTable('15D-n7O5AdsttUF3LfkOhRexS-Q4T78MfXDbUlmsPHRc')
 
-    utils.getTable('1Dk0WYpOKeRoDATgzMkIkFjUcFwNAG5MRn4W7bEyzd0M')
+    # utils.getTable('1Dk0WYpOKeRoDATgzMkIkFjUcFwNAG5MRn4W7bEyzd0M')
 
