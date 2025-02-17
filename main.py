bf02fcf893a1ab3bebd7173a4df448279770f61f
Zelenskiy <zelensk1973@gmail.com>
2025-02-17 20:48:26 +0200

    deploy

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import utils
 
 # Приклад використання:
 if __name__ == "__main__":
-    
+    # p
     utils.getTable('1F6QVr9WNio-_ODmnIlMTSHeSQxLOjgnd0nYB1_z0BeI')
     # utils.getTable('1G1l3J4HHLOItVLYbrPL08ml3TtON_fAULcpecqn0vwM')
     # utils.getTable('15D-n7O5AdsttUF3LfkOhRexS-Q4T78MfXDbUlmsPHRc')
