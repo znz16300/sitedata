2b0713f0fed8a86415b1c2d094b264b8baa60684
Zelenskiy <zelensk1973@gmail.com>
2025-06-29 09:01:21 +0300

    feat: add document table

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ import utils
 # Приклад використання:
 if __name__ == "__main__":
     # Pages
-    utils.getTable('1F6QVr9WNio-_ODmnIlMTSHeSQxLOjgnd0nYB1_z0BeI')
+    # utils.getTable('1F6QVr9WNio-_ODmnIlMTSHeSQxLOjgnd0nYB1_z0BeI')
     # Menu
-    utils.getTable('1G1l3J4HHLOItVLYbrPL08ml3TtON_fAULcpecqn0vwM')
+    # utils.getTable('1G1l3J4HHLOItVLYbrPL08ml3TtON_fAULcpecqn0vwM')
     # Teachers
     # utils.getTable('15D-n7O5AdsttUF3LfkOhRexS-Q4T78MfXDbUlmsPHRc')
     # News
-    utils.getTable('1Dk0WYpOKeRoDATgzMkIkFjUcFwNAG5MRn4W7bEyzd0M')
+    # utils.getTable('1Dk0WYpOKeRoDATgzMkIkFjUcFwNAG5MRn4W7bEyzd0M')
+    # Documents
+    utils.getTable('1O_bJjH8TAHww34uxA51rdyJoX4PaxMGOzL57N8G7H34')
 
