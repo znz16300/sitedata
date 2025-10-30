1e5e27e35a3023bdfd0c8f5d51728d110df2a8a3
Zelenskiy <zelensk1973@gmail.com>
2025-10-30 18:07:32 +0200

    feat: add news

--- main.py
+++ main.py
@@ -2,18 +2,16 @@ import utils
 
 if __name__ == "__main__":
     # Pages
-    utils.getTable('1F6QVr9WNio-_ODmnIlMTSHeSQxLOjgnd0nYB1_z0BeI')
-    utils.getTable('12tqoeJpUs1qa9paI7YuMpZ17nynq44-qqmn0zgoEup4')
+    # utils.getTable('1F6QVr9WNio-_ODmnIlMTSHeSQxLOjgnd0nYB1_z0BeI')
+    # utils.getTable('12tqoeJpUs1qa9paI7YuMpZ17nynq44-qqmn0zgoEup4')
     # Курси
-    utils.getTable('1W6zD4eXSqCFW2iObVuNUyjj_hyS1aPi_tWe7Ce8dxWU')
-    # Menu
-    # utils.getTable('1G1l3J4HHLOItVLYbrPL08ml3TtON_fAULcpecqn0vwM')
+    # utils.getTable('1W6zD4eXSqCFW2iObVuNUyjj_hyS1aPi_tWe7Ce8dxWU')
     # Teachers
-    utils.getTable('15D-n7O5AdsttUF3LfkOhRexS-Q4T78MfXDbUlmsPHRc')
+    # utils.getTable('15D-n7O5AdsttUF3LfkOhRexS-Q4T78MfXDbUlmsPHRc')
     # News
     utils.getTable('1Dk0WYpOKeRoDATgzMkIkFjUcFwNAG5MRn4W7bEyzd0M')
     # Documents
-    utils.getTable('1O_bJjH8TAHww34uxA51rdyJoX4PaxMGOzL57N8G7H34')
+    # utils.getTable('1O_bJjH8TAHww34uxA51rdyJoX4PaxMGOzL57N8G7H34')
     # Schedules
-    utils.getSchedules('1vWepnObCCI_61Ubt4Dlh6nWBjOUICCCrn8MyG-od9Sg')
+    # utils.getSchedules('1vWepnObCCI_61Ubt4Dlh6nWBjOUICCCrn8MyG-od9Sg')
 
