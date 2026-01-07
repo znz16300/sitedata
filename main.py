59ac026a42c079abc879cb44b43d91d6b6a39505
Zelenskiy <zelensk1973@gmail.com>
2026-01-07 19:03:33 +0200

    deploy

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     utils.getTable('1F6QVr9WNio-_ODmnIlMTSHeSQxLOjgnd0nYB1_z0BeI')
     utils.getTable('12tqoeJpUs1qa9paI7YuMpZ17nynq44-qqmn0zgoEup4')
     # Курси
-    utils.getTable('1W6zD4eXSqCFW2iObVuNUyjj_hyS1aPi_tWe7Ce8dxWU')
+    # utils.getTable('1W6zD4eXSqCFW2iObVuNUyjj_hyS1aPi_tWe7Ce8dxWU')
     # Teachers
     utils.getTable('15D-n7O5AdsttUF3LfkOhRexS-Q4T78MfXDbUlmsPHRc')
     # News
